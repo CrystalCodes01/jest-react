1b1f850e4d53a45a11025e0190b13e6a6a059f49
Crystal Maria <crystalmaria@Crystals-MacBook-Air.local>
2018-08-08 14:49:25 -0400

    11 passing

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      error: false,
+    };
+
+    // bind this for decrementCounter and incrementCounter for this.state and this.setState
+    this.decrementCounter = this.decrementCounter.bind(this)
+    this.incrementCounter = this.incrementCounter.bind(this)
+  }
+  decrementCounter() {
+    if (this.state.counter === 0) {
+      this.setState({ error: true });
+    } else {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  }
+
+  incrementCounter() {
+    if (this.state.error) {
+      this.setState({ error: false });
+    }
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    // determine whether error is hidden based on state
+    const errorClass = this.state.error ? '' : 'hidden';
+
+    return (
+      <div data-test="component-app" className='App'>
+        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+            <div data-test="error-message" className={`error ${errorClass}`}>
+                The counter cannot go below 0
+            </div>
+          <div data-test="component-app" className='App'>
+            <button
+              data-test="increment-button"
+              onClick={this.incrementCounter}
+              >
+              Increment counter
+            </button>
+            <button
+              data-test="decrement-button"
+              onClick={this.decrementCounter}
+              >
+              Decrement counter
+            </button>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
